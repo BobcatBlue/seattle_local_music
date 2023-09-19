60111c8e9ecdb0754131a42d2029e003228ae133
Chris <chrismdowning@gmail.com>
2023-09-18 21:56:58 -0700

    Create module for pulling show data; Organize show-data into a dictionary; Just need to expand the data-set from here.

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
 from ticketmaster_shows import get_shows
+import pandas as pd
 
-vId_crocodile = "KovZpZA1vFtA"
+
+df = pd.read_csv("Seattle_ticketmaster_venues - Sheet1.csv")
+
+venue_ids = []
+
+# Create a dictionary in which all of the information will be stored, sorted by category
+shows = {
+    "venues": [],
+    "bands": [],
+    "dates": [],
+    # "image links": [],
+    # "show links": [],
+}
+
+venues = [row["Venue Name"] for index, row in df.iterrows()]
+print(venues)  # this is a checker
+
+# Fill the dictionary with info
+for index, row in df.iterrows():
+    show_info = get_shows(row["Venue Name"], row["vID"])
+    for key, value in show_info:
+        shows[key].append(value)
 
 
-print(get_shows(vId_crocodile))
+print(shows)
+
+# for venue, ids in show_ids:
+
+
+print(show_info)  # this is a checker
+
--- ticketmaster_shows.py
+++ ticketmaster_shows.py
@@ -5,14 +5,19 @@ import json
 
 API_KEY = os.getenv("ticketmaster_api_1")
 
-def get_shows(venueId):
-    url = f"https://app.ticketmaster.com/discovery/v2/events.json?venueId={venueId}&apikey={API_KEY}"
+
+def get_shows(venue_name, venueId):
+    url = f"https://app.ticketmaster.com/discovery/v2/events.json?venueId={venueId}" \
+          f"&apikey={API_KEY}"
+    print(url)
     response = requests.get(url)
     show_data = response.json()
 
-    name = show_data["_embedded"]["events"][0]["name"]
-    date = show_data["_embedded"]["events"][0]["dates"]["start"]["localDate"]
+    venue_name = ["venues", venue_name]
+    band = ["bands", show_data["_embedded"]["events"][0]["name"].split(", ")]
+    date = ["dates", show_data["_embedded"]["events"][0]["dates"]["start"]["localDate"]]
 
-    show_info = [name, date]
+    info = [venue_name, band, date]
 
-    return(show_info)
+    return info
+
